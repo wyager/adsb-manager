555f44cc23942339efc0fb0628dc54d83d402702
will <will@yager.io>
2020-02-27 18:05:50 +0800

    ok

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,4 +9,5 @@ run :: IO ()
 run = do
     P.runProcess_ "rtl_biast -b 1"
     P.withProcessWait_ "dump1090 --device-type rtlsdr --net --forward-mlat" $ \_dump1090 ->
-        return ()
+        P.withProcessWait_ "socat -u TCP:localhost:30005 TCP:feed.adsbexchange.com:30005" $ \_socat ->
+            return ()
