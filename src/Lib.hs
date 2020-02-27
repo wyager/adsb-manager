c3b1e968bfd1a75e897d196738721d01dcf15e13
will <will@yager.io>
2020-02-27 18:15:25 +0800

    delay

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,12 +2,13 @@ module Lib
     ( run
     ) where
     
-import qualified System.Process.Typed  as P
-
+import qualified System.Process.Typed as P
+import qualified Control.Concurrent as C
 
 run :: IO ()
 run = do
     P.runProcess_ "rtl_biast -b 1"
-    P.withProcessWait_ "dump1090 --device-type rtlsdr --net --forward-mlat" $ \_dump1090 ->
-        P.withProcessWait_ "socat -u TCP:localhost:30005 TCP:feed.adsbexchange.com:30005" $ \_socat ->
+    P.withProcessWait_ "dump1090 --device-type rtlsdr --net --forward-mlat" $ \_dump1090 -> do
+        C.threadDelay (5*1000*1000)
+        P.withProcessWait_ "socat -u TCP:localhost:30005 TCP:feed.adsbexchange.com:30005" $ \_socat -> do
             return ()
