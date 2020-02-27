959be6d14ac399495a6339c2727a0de1bf3f7c55
will <will@yager.io>
2020-02-27 17:59:44 +0800

    ok

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( run
     ) where
+    
+import qualified System.Process.Typed  as P
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+run :: IO ()
+run = do
+    P.runProcess_ "rtl_biast -b 1"
+    P.withProcessWait_ "dump1090 --device-type rtlsdr --net --forward-mlat" $ \_dump1090 ->
+        return ()
